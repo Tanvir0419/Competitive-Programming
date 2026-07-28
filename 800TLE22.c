64627fd22fa64e9b8aa511dce6024d2632c0aa87
Tanvir0419 <kh16981@gmail.com>
2026-07-28 09:51:26 +0600

    Solving 1 cp per 2 days because of academic pressure

---
+++ 800TLE22.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++)
+    {
+        int n, k;
+        scanf("%d%d", &n, &k);
+        if (n % 2 == 0 || k % 2 != 0)
+        {
+            printf("Yes\n");
+        }
+        else
+        {
+            printf("No\n");
+        }
+    }
+    return 0;
+}
